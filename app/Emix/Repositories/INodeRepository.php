f9dc33388418aff9cbb65a98c19248fbcf4e8a0d
Jönsson, Johan (Bisnode) <johan.jonsson@bisnode.se>
2014-06-29 17:35:27 +0200

    Added some docblocks

--- app/Emix/Repositories/INodeRepository.php
+++ app/Emix/Repositories/INodeRepository.php
@@ -1,13 +1,32 @@
 <?php namespace Emix\Repositories;
 
+/**
+ * Interface INodeRepository
+ * @package Emix\Repositories
+ */
 interface INodeRepository {
 
+    /**
+     * @return \Collection
+     */
     public function all();
 
+    /**
+     * @param string $id
+     * @return \Emix\Node
+     */
     public function find($id);
 
+    /**
+     * @param $id
+     * @return \Emix\Node
+     */
     public function findWithContainers($id);
 
+    /**
+     * @param $name
+     * @return \Emix\Node
+     */
     public function findByName($name);
 
 } 
